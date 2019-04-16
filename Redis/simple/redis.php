cd02f8cd876035a12871daa10a26876c7e347569
hermite <hermite@nongline.com>
2019-04-16 16:15:46 +0800

    2

--- Redis/simple/redis.php
+++ Redis/simple/redis.php
@@ -5,4 +5,10 @@
  * Date: 2019/4/16
  * Time: 15:55
  */
-
+//实例化redis
+$redis = new Redis();
+//连接
+$redis->connect('127.0.0.1', 6379);
+//检测是否连接成功
+echo "Server is running: " . $redis->ping();
+// 输出结果 Server is running: +PONG
