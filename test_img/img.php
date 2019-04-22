4d84091afae005def390e126312c599f658efeeb
hermite <hermite@nongline.com>
2019-04-22 14:40:54 +0800

    2

---
+++ test_img/img.php
@@ -0,0 +1,8 @@
+<?php
+$z =0;
+for ($i=1 ;$i<=10 ;$i++){
+    for ($h =1;$h <=15 ;$h++){
+        echo "<img src='https://devstatic.nongline.com/upload/sp/cate/201804/18/cate_image/fb20fe050b00658d02c379d5afe2777e.jpg?id=$z'>";
+        $z++;
+    }
+}
