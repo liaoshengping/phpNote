82873c71dd63b55d8b305b2ea5e69cfab1384e6c
廖 <lsp@nongline.com>
2019-05-27 09:32:40 +0800

    3

--- jiguang-demo/jiguang/examples/chatroom_examples.php
+++ jiguang-demo/jiguang/examples/chatroom_examples.php
@@ -2,7 +2,7 @@
 require __DIR__ . '/config.php';
 use JMessage\IM\ChatRoom;
 
-$room = new ChatRoom($jm);//
+$room = new ChatRoom($jm);
 
 $response = $room->listAll(10);
 print_r($response);
--- jiguang-demo/jiguang/examples/user_examples.php
+++ jiguang-demo/jiguang/examples/user_examples.php
@@ -7,5 +7,13 @@ require __DIR__ . '/config.php';
 use JMessage\IM\User;
 //注册
 $user = new User($jm);
-//$res =$user->register('liaosp3','liaosp3');
-//var_dump($res);
+$res =$user->register('liaosp4','liaosp4','bb','我是圣平4');
+//获取用户信息
+$res =$user->show('liaosp4');
+
+//$options =[
+//    'nickname'=>'我是修改的',
+//];
+//$res =$user->update('liaosp4',$options);
+
+var_dump($res);
