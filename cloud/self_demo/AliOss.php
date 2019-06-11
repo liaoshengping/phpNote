6a1f8f42c3f99f0a185d5714f9ad6f78cbc1e7d4
廖 <lsp@nongline.com>
2019-06-11 17:09:31 +0800

    '2'

---
+++ cloud/self_demo/AliOss.php
@@ -0,0 +1,23 @@
+<?php
+
+
+class AliOss
+{
+    protected $key;
+    protected $secret;
+    protected $buff;
+    public function __construct($key,$secret,$buff)
+    {
+        $this->key= $key;
+        $this->secret= $secret;
+        $this->buff = $buff;
+        return $this;
+    }
+    public static function create()
+    {
+        return new self(...func_get_args());
+    }
+    public function upload($path){
+        echo "用 key:".$this->key.'上传文件'.$path;
+    }
+}
---
+++ cloud/self_demo/Upload.php
@@ -0,0 +1,85 @@
+<?php
+
+require_once ('Loder.php');
+
+/**
+ * Class Upload
+ * @static $upload
+ */
+class Upload
+{
+    /**
+     * 实例池
+     * @var array
+     */
+    protected static $example = [];
+    /**
+     * 目前使用的驱动
+     * @var bool
+     */
+    protected static $driver = 'Qiniu';
+    /**
+     * 接口
+     * @var array
+     *
+     */
+    protected static $interfaces = [
+        'Qiniu'=>Qiniu::class,
+        'Alioss'=>AliOss::class,
+    ];
+    /**
+     * 设置接口
+     * @param $name
+     * @param null $class
+     * @return int|null
+     */
+    public static function set_interface($name,$class = null)
+    {
+        if($class === null){
+            return self::$interfaces = array_push(self::$interfaces,$name);
+        }
+        return self::$interfaces[$name] = $class;
+    }
+
+    /**
+     * 装饰者(指向制定的 存储器)
+     * @param $name
+     * @param $arguments
+     * @return mixed
+     */
+    public static function __callStatic($name, $arguments)
+    {
+        return self::$example[self::$driver] -> $name(...$arguments);
+    }
+
+    /**
+     * 启动上传组件
+     */
+    public static function start()
+    {
+        # 判断驱动是否存在
+        if(!isset(self::$interfaces[self::$driver])){
+            return false;
+        }
+        return self::$example[self::$driver] = self::$interfaces[self::$driver]::create(...func_get_args());
+    }
+
+    /**
+     * 设置文件上传驱动
+     * @param null | string $name
+     * @return bool|null
+     */
+    public static function set_driver($name = null)
+    {
+        return self::$driver = ($name===null)?self::$driver:$name;
+    }
+
+
+}
+
+Upload::set_driver('Alioss');
+$key = 'kakkaka';
+$secret = 'kakkak';
+$buff = 'sldfjdskf';
+Upload::start($key,$secret,$buff);
+Upload::upload('dingdangmao.jpg');
