91aa6f1e6ec2f90e11004e687659027cb1da34ab
廖 <lsp@nongline.com>
2019-08-07 15:46:39 +0800

    '2'

---
+++ auto/model/model.php
@@ -0,0 +1,21 @@
+<?php
+$arr = [
+    'id',
+    'name',
+];
+$data ='';
+
+
+foreach($arr as $key=>$value){
+    $data.= 'public static function findOneBy'.ucfirst($value).PHP_EOL;
+    $data.='{'.PHP_EOL;
+        $data.='static::find';
+
+    $data .= PHP_EOL.'}'.PHP_EOL;
+}
+
+foreach($arr as $key=>$value){
+    $data.= 'public static function getListBy'.ucfirst($value).PHP_EOL;
+}
+
+echo $data;
