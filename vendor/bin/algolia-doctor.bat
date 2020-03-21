d56475b03a7ceaedd020948157be0fd3f91f4487
liaosp <1194008361@qq.com>
2020-03-21 15:24:01 +0800

    3

--- vendor/bin/algolia-doctor.bat
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-setlocal DISABLEDELAYEDEXPANSION
-SET BIN_TARGET=%~dp0/../algolia/algoliasearch-client-php/bin/algolia-doctor
-php "%BIN_TARGET%" %*
--- vendor/liaosp/ali_open/src/interfaces/Provider.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Liaosp\AliOpen\interfaces;
-
-use Liaosp\AliOpen\core\Container;
-
-interface Provider
-{
-    public function serviceProvider(Container $container);
-}
