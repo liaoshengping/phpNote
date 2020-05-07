ed697d9c1fba09b3891bf4f29226f859fd59c916
廖 <1194008361@qq.com>
2020-05-07 10:10:01 +0800

    3

--- ai/index.php
+++ ai/index.php
@@ -9,14 +9,11 @@ $samples = [
     ['香烟','打火机','炸鸡','啤酒','吸毒'],
     ['香烟','打火机','炸鸡','可乐','吸毒'],
     ['可乐','炸鸡','薯条','可乐','吸毒','炸鸡翅'],
-
 ];
 use Phpml\Association\Apriori;
-$associator  =  new  Apriori($support  =  0.2,  $confidence  =  0.2);
+$associator = new  Apriori($support  =  0.2,  $confidence  =  0.2);
 $associator->train($samples,  []);
 $associator->getRules();
-
 $res = $associator->predict(['薯条']);
-
 var_dump($res);exit;
 
