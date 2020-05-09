9a8546ff6525b224075d7d68cf647b8d9e9092b2
廖 <1194008361@qq.com>
2020-05-09 14:45:00 +0800

    2

--- tool/截图/save.php
+++ tool/截图/save.php
@@ -1,5 +1,5 @@
 <?php
 
-$data = file_get_contents('http://127.0.0.1:8181/?a=http://www.jq22.com/yanshi23026');
+$data = file_get_contents('http://127.0.0.1:8181/?a=http://www.jq22.com/demo/jqueryEcharts202004092216/');
 
 file_put_contents(__DIR__.'/image.png', base64_decode($data));
