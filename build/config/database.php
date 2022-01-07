9d21eb290ac07238368f1469629e024d309cd10a
liaosp <1194008361@qq.com>
2022-01-07 18:09:46 +0800

    2

--- build/config/database.php
+++ build/config/database.php
@@ -4,9 +4,9 @@
 namespace config;
 
 return [
-    "host" => xenv"host","192.168.205.22"),
-    "database" => xenv"yibage",'yibage'),
-    "port" => xenv"port","3306"),
-    "username" => xenv"username","root"),
-    "password" => xenv"password","123456")
+    "host" => xenv("host","192.168.205.22"),
+    "database" => xenv("yibage",'yibage'),
+    "port" => xenv("port","3306"),
+    "username" => xenv("username","root"),
+    "password" => xenv("password","123456")
 ];
--- build/config/edu-config.php
+++ build/config/edu-config.php
@@ -29,11 +29,11 @@ return [
     /**
      * 数据库信息
      */
-    "host" => xenv"host", "192.168.205.22"),
+    "host" => xenv("host", "192.168.205.22"),
     "database" => 'edu-manager',
-    "port" => xenv"port", "3306"),
-    "username" => xenv"username", "root"),
-    "password" => xenv"password", "123456"),
+    "port" => xenv("port", "3306"),
+    "username" => xenv("username", "root"),
+    "password" => xenv("password", "123456"),
 
 
     "fornt" => "",
