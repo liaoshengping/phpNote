adbe998d141d2bf93e2dd70b758eb531de5708fe
廖圣平 <1194008361@qq.com>
2022-05-09 22:56:41 +0800

    fix

--- selenium/test.php
+++ selenium/test.php
@@ -1,15 +1,27 @@
 <?php
 
-namespace Facebook\WebDriver;
+use Facebook\WebDriver\Remote\RemoteWebDriver;
 include ("../vendor/autoload.php");
 
-use Facebook\WebDriver\Chrome\ChromeOptions;
-use Facebook\WebDriver\Remote\DesiredCapabilities;
-use Facebook\WebDriver\Remote\RemoteWebDriver;
+$serverUrl = 'http://localhost:4444';
 
-$wd_host = 'http://127.0.0.1:4444/wd/hub';
-$driver = RemoteWebDriver::create($wd_host, DesiredCapabilities::chrome());
-$driver->get("http://baidu.com");
+// Chrome
+$driver = RemoteWebDriver::create($serverUrl, \Facebook\WebDriver\Remote\DesiredCapabilities::chrome());
+
+
+for ($day=0;$day<32;$day++){
+    $date = \Carbon\Carbon::parse('2022-04-01')->addDays($day)->timestamp;
+    $driver->get('https://app.diandian.com/rank/android/2-201-0-75-901977?time='.$date.'000&timetype=custom');
+    for ($i = 1; $i < 60; $i++) {
+        sleep(1);
+        $driver->executeScript('window.scrollTo(0,document.body.scrollHeight);');
+        $driver->executeScript('window.scrollTo(0,0);');
+        $driver->executeScript('window.scrollTo(0,document.body.scrollHeight);');
+    }
+    $data =$driver->getPageSource();
+    file_put_contents(__DIR__.'/4/'.$date.'.html',$data);
+}
+
 
 
 ?>
