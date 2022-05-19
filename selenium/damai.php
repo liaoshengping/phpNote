e2dc8f84a9efb4e1dbde9d444671669de3f4aa26
廖圣平 <1194008361@qq.com>
2022-05-19 18:55:52 +0800

    fix

---
+++ selenium/damai.php
@@ -0,0 +1,30 @@
+<?php
+
+use Facebook\WebDriver\Remote\RemoteWebDriver;
+include ("../vendor/autoload.php");
+
+$serverUrl = 'http://localhost:4444';
+
+// Chrome
+$driver = RemoteWebDriver::create($serverUrl, \Facebook\WebDriver\Remote\DesiredCapabilities::chrome());
+
+
+for ($day=0;$day<32;$day++){
+    $date = \Carbon\Carbon::parse('2022-04-01')->addDays($day)->timestamp;
+    $driver->get('https://app.diandian.com/rank/android/2-201-0-75-901977?time='.$date.'000&timetype=custom');
+    for ($i = 1; $i < 45; $i++) {
+        sleep(1);
+        $driver->executeScript('window.scrollTo(0,document.body.scrollHeight);');
+        $driver->executeScript('window.scrollTo(0,0);');
+        $driver->executeScript('window.scrollTo(0,document.body.scrollHeight);');
+        $driver->executeScript('window.scrollTo(0,0);');
+        sleep(1);
+        $driver->executeScript('window.scrollTo(0,document.body.scrollHeight);');
+    }
+    $data =$driver->getPageSource();
+    file_put_contents(__DIR__.'/4/'.$date.'.html',$data);
+}
+
+
+
+?>
