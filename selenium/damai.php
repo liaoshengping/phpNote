3e394e162277a7d565e4b4c069009062378ce352
廖圣平 <1194008361@qq.com>
2022-12-06 18:02:58 +0800

    点击事件

--- selenium/damai.php
+++ selenium/damai.php
@@ -11,7 +11,7 @@ $capabilities  = \Facebook\WebDriver\Remote\DesiredCapabilities::chrome();
 
 
 $option = new \Facebook\WebDriver\Chrome\ChromeOptions();
-$option->addArguments();
+//$option->addArguments();
 
 $capabilities->setPlatform(\Facebook\WebDriver\WebDriverPlatform::WINDOWS);
 $capabilities->setCapability('userAgent', 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36');
@@ -19,20 +19,41 @@ $capabilities->setCapability('user-agent', 'Mozilla/5.0 (Windows NT 6.1) AppleWe
 // Chrome
 $driver = RemoteWebDriver::create($serverUrl, $capabilities);
 
-
+//可学习点击事件
+//https://github.com/luanloose/api-mc-experiencia/blob/8f5d31d8017a27f3d452501a303689891dd94a64/app/Http/Controllers/McDonalds.php#L188
 
 
 //for ($day=0;$day<32;$day++){
 //    $date = \Carbon\Carbon::parse('2022-04-01')->addDays($day)->timestamp;
-    $driver->get('https://www.qimai.cn/rank/index/brand/free/device/iphone/country/cn/genre/1000000000');
+    $driver->get('https://kzurl02.cn/dCKdK');
     for ($i = 1; $i < 10000; $i++) {
+        echo '到了';
+        sleep(2);
+
+
+
+        $click = $driver->findElement(
+            \Facebook\WebDriver\WebDriverBy::xpath('//div[contains(@class, "buy-btn")]['.$i.']')
+//            \Facebook\WebDriver\WebDriverBy::
+        );
+
+        $click->click();
+
+        sleep(2);
+        $driver->findElement(
+            \Facebook\WebDriver\WebDriverBy::xpath('//span[contains(text(), "复制口令")]')
+        )->click();
         sleep(1);
-        $driver->executeScript('window.scrollTo(0,document.body.scrollHeight);');
-        $driver->executeScript('window.scrollTo(0,0);');
-        $driver->executeScript('window.scrollTo(0,document.body.scrollHeight);');
-        $driver->executeScript('window.scrollTo(0,0);');
-        sleep(1);
-        $driver->executeScript('window.scrollTo(0,document.body.scrollHeight);');
+
+        $data =$driver->getPageSource();
+        file_put_contents(__DIR__.'/damai/'.$i.'.html',$data);
+
+//        $driver->executeScript('window.scrollTo(0,document.body.scrollHeight);');
+//        $driver->executeScript('window.scrollTo(0,0);');
+//        $driver->executeScript('window.scrollTo(0,document.body.scrollHeight);');
+//        $driver->executeScript('window.scrollTo(0,0);');
+//        sleep(1);
+//        $driver->executeScript('window.scrollTo(0,document.body.scrollHeight);');
     }
     $data =$driver->getPageSource();
     file_put_contents(__DIR__.'/damai/.html',$data);
