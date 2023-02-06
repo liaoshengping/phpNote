cd62a56715f0e2454522c63c57069a71e9264f1a
廖圣平 <1194008361@qq.com>
2023-02-06 15:02:23 +0800

    updatmatch

--- selenium/jingji/match.php
+++ selenium/jingji/match.php
@@ -5,10 +5,21 @@ $jingjibao = json_decode(file_get_contents(__DIR__.'/json/jingjibao.json'),true)
 $ray = json_decode(file_get_contents(__DIR__.'/json/ray.json'),true);
 
 
-foreach ($jingjibao as $item){
-    echo $item['leftTeamName'].'-'.$item['rightTeamName'].PHP_EOL;
-}
+$result = [];
+
+$rays = [];
+
 
 foreach ($ray as $item){
-    echo $item['leftTeamName'].'-'.$item['rightTeamName'].PHP_EOL;
+    $rays[] =  $item['leftTeamName'].'-'.$item['rightTeamName'];
 }
+
+foreach ($jingjibao as $item){
+
+    if (in_array($item['leftTeamName'].'-'.$item['rightTeamName'],$rays)){
+        $result[] = $item['leftTeamName'].'-'.$item['rightTeamName'];
+    }
+}
+
+var_dump(implode(',',$result));
+
