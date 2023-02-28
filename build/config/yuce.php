2f57353f8dfb5a3814708337f86efed9c527ac0c
廖圣平 <1194008361@qq.com>
2023-02-28 09:30:01 +0800

    fix

--- build/config/yuce.php
+++ build/config/yuce.php
@@ -205,6 +205,61 @@ return [
 
             ]
         ],
+        'order' => [
+            'name' => '订单',
+            'request_method' => 'form',//form表单 json (Json Body的形式),
+            'fields' => [
+                ''
+            ],
+            'input' => [
+
+            ],
+            'is_auth' => false,//只可以获取自己的信息，结合auth_user_id 使用
+            'no_cover_admin' => true,//创建laravel-admin 后台数据不可以强制覆盖
+
+            'controller_actions' => ['create','list','show'],
+//            ['create','list','edit','show','delete'];
+            'create_input' => [], //创建需要的字段如果为空取上面的
+
+            'edit_input' => [],//编辑需要的字段 如果为空取上面的
+
+            'relations' => [
+
+                [
+                    'relation' => "hasOne",
+                    'tables' => [
+                        [
+                            'table_name' => 'admin_user',
+                            'target' => 'id', //目标表中的字段
+                            'origin' => 'admin_id',//本表的字段
+                            'list_show' => true,
+                            'list_exist' => false,
+                            'one_show' => true,
+                            'create_relation' => false,//创建时，是否可以关联添加
+                        ],
+
+                    ],
+
+                ],
+                [
+                    'relation' => "hasOne",
+                    'tables' => [
+                        [
+                            'table_name' => 'users',
+                            'target' => 'id', //目标表中的字段
+                            'origin' => 'user_id',//本表的字段
+                            'list_show' => true,
+                            'list_exist' => false,
+                            'one_show' => true,
+                            'create_relation' => false,//创建时，是否可以关联添加
+                        ],
+
+                    ],
+
+                ]
+
+            ]
+        ],
         'plan_code' => [
             'name' => '方案数据',
             'request_method' => 'form',//form表单 json (Json Body的形式),
