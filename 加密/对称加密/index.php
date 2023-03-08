ce6903191522d8fce60e8c54891519fb8a57bd2e
廖圣平 <1194008361@qq.com>
2023-03-08 11:37:53 +0800

    fix

---
+++ 加密/对称加密/index.php
@@ -0,0 +1,36 @@
+<?php
+function encode($string = '', $skey = 'tsxcc') {
+
+    $strArr = str_split(base64_encode($string));
+
+    $strCount = count($strArr);
+
+    foreach (str_split($skey) as $key => $value)
+
+        $key < $strCount && $strArr[$key].=$value;
+
+    return str_replace(array('=', '+', '/'), array('O0O0O', 'o000o', 'oo00o'), join('', $strArr));
+
+}
+
+//解密
+
+function decode($string = '', $skey = 'tsxcc') {
+
+    $strArr = str_split(str_replace(array('O0O0O', 'o000o', 'oo00o'), array('=', '+', '/'), $string), 2);
+
+    $strCount = count($strArr);
+
+    foreach (str_split($skey) as $key => $value)
+
+        $key <= $strCount && isset($strArr[$key]) && $strArr[$key][1] === $value && $strArr[$key] = $strArr[$key][0];
+
+    return base64_decode(join('', $strArr));
+
+}
+
+$data = encode(json_encode(['pid' => 1,'rid' => 2]));
+
+echo $data.PHP_EOL;
+
+echo '解密：'.decode($data);
