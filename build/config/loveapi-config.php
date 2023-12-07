bb297351da498cc321d4af117fc3849a453e8552
廖圣平 <1194008361@qq.com>
2023-12-07 16:08:57 +0800

    fix

--- build/config/loveapi-config.php
+++ build/config/loveapi-config.php
@@ -99,19 +99,63 @@ return [
      * 数据差异性
      */
     "tables" => [
-        'goods' => [
-            'name' => '商品管理',
+        'user' => [
+            'name' => '用户管理',
             'request_method' => 'form',//form表单 json (Json Body的形式),
             'fields' => [
                 ''
             ],
-            'input' => [
-                'code_type'
-            ],
-            'is_auth' => true,//只可以获取自己的信息，结合auth_user_id 使用
+            'input' => [],
+            'is_auth' => false,//只可以获取自己的信息，结合auth_user_id 使用
             'no_cover_admin' => true,//创建laravel-admin 后台数据不可以强制覆盖
-            'create_input' => ['name','code_num','plan_type'], //创建需要的字段如果为空取上面的
+            'create_input' => [], //创建需要的字段如果为空取上面的
             'controller_actions' => ['show'],
+//            ['create','list','edit','show','delete'];
+//            'create_input' => [], //创建需要的字段如果为空取上面的
+
+            'edit_input' => [],//编辑需要的字段 如果为空取上面的
+            /**
+             * [
+             *   name:'liaosp',
+             *   plan_code:[
+             *      {
+             *
+            'name':'liaosp',
+             * }
+             * ]
+             * ]
+             */
+            'relations' => [
+
+//                [
+//                    'relation' => "hasMany",
+//                    'tables' => [
+//                        [
+//                            'table_name' => 'plan_code',
+//                            'target' => 'plan_id', //目标表中的字段
+//                            'origin' => 'id',//本表的字段
+//                            'list_show' => true,
+//                            'list_exist' => false,
+//                            'one_show' => true,
+//                            'create_relation' => false,//创建时，是否可以关联添加
+//                        ],
+//                    ],
+//                ],
+
+            ]
+        ],
+
+        'order' => [
+            'name' => '订单管理',
+            'request_method' => 'form',//form表单 json (Json Body的形式),
+            'fields' => [
+                ''
+            ],
+            'input' => [],
+            'is_auth' => false,//只可以获取自己的信息，结合auth_user_id 使用
+            'no_cover_admin' => true,//创建laravel-admin 后台数据不可以强制覆盖
+            'create_input' => [], //创建需要的字段如果为空取上面的
+            'controller_actions' => ['list','show'],
 //            ['create','list','edit','show','delete'];
 //            'create_input' => [], //创建需要的字段如果为空取上面的
 
