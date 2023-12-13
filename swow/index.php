995bb418a4d3bdf1521ad12661ccac04f71f0217
廖圣平 <1194008361@qq.com>
2023-12-13 23:28:43 +0800

    fix

--- swow/index.php
+++ swow/index.php
@@ -1,1 +1,69 @@
 <?php
+
+
+use Swow\Coroutine;
+use Swow\Sync\WaitGroup;
+
+
+function work3($url): void
+{
+    echo '处理了一个';
+    file_put_contents(md5($url) . '.html', file_get_contents($url));
+}
+
+$wg = new WaitGroup();
+
+$arr = [
+    'http://www.baidu.com',
+    'http://www.baidu.com',
+    'http://www.baidu.com',
+    'http://www.baidu.com',
+    'http://www.baidu.com',
+    'http://www.baidu.com',
+    'http://www.baidu.com',
+    'http://www.baidu.com',
+    'http://www.baidu.com',
+    'http://www.baidu.com',
+    'http://www.baidu.com',
+    'http://www.baidu.com',
+    'http://www.baidu.com',
+    'http://www.baidu.com',
+    'http://www.baidu.com',
+    'http://www.baidu.com',
+    'http://www.baidu.com',
+    'http://www.baidu.com',
+    'http://www.baidu.com',
+    'http://www.baidu.com',
+    'http://www.baidu.com',
+    'http://www.baidu.com',
+    'http://www.baidu.com',
+    'http://www.baidu.com',
+    'http://www.baidu.com',
+    'http://www.baidu.com',
+    'http://www.baidu.com',
+    'http://www.baidu.com',
+    'http://www.baidu.com',
+    'http://www.baidu.com',
+    'http://www.baidu.com',
+    'http://www.baidu.com',
+    'http://www.baidu.com',
+    'http://www.baidu.com',
+    'http://www.baidu.com',
+//    'https://jj.dmjvip.com/agent/channel/list',
+//    'https://jj.dmjvip.com/agent/channel/list',
+];
+
+// $wg->add(3); // 这么写也可以
+
+foreach ($arr as $item) {
+    $wg->add();
+    Coroutine::run(static function () use ($item, $wg): void {
+        work3($item);
+        $wg->done();
+    });
+}
+
+
+echo "Wait...\n";
+$wg->wait();
+echo "Done\n";
---
+++ ups.php
@@ -0,0 +1,40 @@
+<?php
+// USPS API Endpoint
+$apiUrl = 'https://secure.shippingapis.com/ShippingAPI.dll';
+
+// USPS API Credentials
+$userId = '9SIMPL93B2765';
+$password = 'ON307255MB2264Y';
+
+// Barcode Number of the original label
+$barcodeNumber = '420322579200190353014500000024';
+
+// Build the XML request payload
+$xmlRequest = <<<XML
+<eVSCancelRequest USERID="{$userId}" PASSWORD="{$password}">
+    <BarcodeNumber>{$barcodeNumber}</BarcodeNumber>
+</eVSCancelRequest>
+XML;
+
+// Build the full API request URL
+$requestUrl = $apiUrl . '?API=eVSCancel&XML=' . urlencode($xmlRequest);
+
+// Set up cURL request
+$ch = curl_init($requestUrl);
+
+// Set cURL options
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+// Execute cURL request
+$response = curl_exec($ch);
+
+// Check for errors
+if (curl_errno($ch)) {
+    echo 'Error: ' . curl_error($ch);
+} else {
+    // Process the response
+    echo 'Response: ' . $response;
+}
+
+// Close cURL session
+curl_close($ch);
